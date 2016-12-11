9bd15f75c69b4988e3907225defd7eef8cf0c5f7
tzing <tzingshih@gmail.com>
2016-12-11 17:59:43 +0800

    init repo

---
+++ lib-appmate/src/main/java/nctu/fintech/appmate/Database.java
@@ -0,0 +1,10 @@
+package nctu.fintech.appmate;
+
+import java.net.URL;
+
+/**
+ *  資料庫
+ */
+public class Database {
+
+}
---
+++ lib-appmate/src/main/java/nctu/fintech/appmate/Table.java
@@ -0,0 +1,8 @@
+package nctu.fintech.appmate;
+
+/**
+ * Created by tzu-ting on 2016/12/11.
+ */
+
+public class Table extends Database {
+}
