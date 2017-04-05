264993e12b60526d3cf49fcc93e5d4cb5578c8b7
Tzu-ting <tzing@outlook.com>
2017-04-05 19:56:39 +0800

    bug fix & add comment

--- lib/src/main/java/nctu/fintech/appmate/core/Core.java
+++ lib/src/main/java/nctu/fintech/appmate/core/Core.java
@@ -1,31 +1,38 @@
 package nctu.fintech.appmate.core;
 
 import android.util.Base64;
-import android.util.Log;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
-import okhttp3.internal.Util;
 
 /**
- * Created by tzu-ting on 2017/4/5.
+ * A class which handles the connection parameters. This class support method chaining.
  */
 public class Core {
 
+    /**
+     * the http client.
+     */
     private static OkHttpClient mClient = new OkHttpClient();
 
+    /**
+     * a {@link URL} refer to the specific resource
+     */
     private URL mRoot;
-    private String mAuth;
 
     /**
-     * @name Constructor
-     * @{
+     * the string to set as authentication header if needed
      */
+    private String mAuth;
 
     /**
      * create a core instance
@@ -33,13 +40,14 @@ public class Core {
      * @param root_url a url path specified to the api root
      * @throws MalformedURLException on url path is malformed
      */
-    public Core(String root_url) throws MalformedURLException {
-        // modify format
-        if (!root_url.endsWith("//")) {
-            root_url += '/';
-        }
+    public Core(String root_url) {
+        root_url = prettifyUrl(root_url);
 
-        mRoot = new URL(root_url);
+        try {
+            mRoot = new URL(root_url);
+        } catch (MalformedURLException e) {
+            throw new IllegalArgumentException("not a valid url", e);
+        }
     }
 
     /**
@@ -52,11 +60,6 @@ public class Core {
         this.mAuth = other.mAuth;
     }
 
-    /**@}
-     *
-     * @name property changer
-     * @{*/
-
     /**
      * setup authentication
      *
@@ -66,7 +69,7 @@ public class Core {
      */
     public Core useAuth(String username, String password) {
         String pair = username + ":" + password;
-        String encoded_pair = Base64.encodeToString(pair.getBytes(), Base64.DEFAULT);
+        String encoded_pair = Base64.encodeToString(pair.getBytes(), Base64.DEFAULT).trim();
         mAuth = "Basic " + encoded_pair;
         return this;
     }
@@ -77,26 +80,46 @@ public class Core {
      * @param path relative path to the original path
      * @throws MalformedURLException
      */
-    public Core cd(String path) throws MalformedURLException {
-        // modify format
-        if (!path.endsWith("//")) {
-            path += '/';
-        }
+    public Core cd(String path) {
+        path = prettifyUrl(path);
 
-        mRoot = new URL(mRoot, path);
+        try {
+            mRoot = new URL(mRoot, path);
+        } catch (MalformedURLException e) {
+            throw new IllegalArgumentException("not a valid url", e);
+        }
         return this;
     }
 
     /**
-     * @}
+     * to ensure the end of URL string is a slash
+     *
+     * @param url url string
+     * @return url sting
      */
+    private String prettifyUrl(String url) {
+        if (!url.endsWith("/")) {
+            url += '/';
+        }
+        return url;
+    }
 
-    public class Request {
+    /**
+     * A class handles the connection process. This class support method chaining.
+     * <p>
+     * This class does not have public constructor. To create a instance use {@link Core#createConnection(URL)}.
+     * <p>
+     */
+    public class Connection {
 
-        private okhttp3.Request.Builder mBuilder;
+        private Request.Builder mBuilder;
 
-        Request(URL url) {
-            mBuilder = new okhttp3.Request.Builder()
+        /**
+             * create a {@link  Connection} instance
+             * @param url the url refer to the resource which you want to query
+             */
+        Connection(URL url) {
+            mBuilder = new Request.Builder()
                     .url(url)
                     .addHeader("Accept", "application/json")
                     .addHeader("Accept-charset", "utf-8");
@@ -106,35 +129,81 @@ public class Core {
             }
         }
 
-        public Request method(String method) throws IOException {
-            mBuilder.method(method, Util.EMPTY_REQUEST);
+        /**
+             * setup request method
+             * @param method request method
+             * @return itself
+             */
+        public Connection method(String method) {
+            mBuilder.method(method, null);
             return this;
         }
 
-        public Request method(String method, RequestBody body) throws IOException {
+        /**
+             * setup request method
+             * @param method request method
+             * @param body request body
+             * @return itself
+             */
+        public Connection method(String method, RequestBody body) {
             mBuilder.method(method, body);
             return this;
         }
 
+        /**
+             * get response
+             *
+             * @return response body
+             * @throws IOException
+             */
         public String getResponse() throws IOException {
-            Response response = mClient.newCall(mBuilder.build()).execute();
+            assert mBuilder != null;
+
+            // build request
+            Request request = mBuilder.build();
+            mBuilder = null;
+
+            // get response
+            Response response = mClient.newCall(request).execute();
             String body = response.body().string();
 
+            // handle result
             if (!response.isSuccessful()) {
-                Log.e(Core.class.getClass().getName(), "unexpected response code: " + response.code());
-                Log.i(Core.class.getClass().getName(), "response body: " + body);
+                throw new UnexpectedResponseException(response.code(), response.message(), body);
             }
 
             return body;
         }
+
+        /**
+             * get response, and cast response body to {@link JsonObject}
+             * @return response body
+             * @throws IOException
+             */
+        public JsonObject getResponseAsJson() throws IOException {
+            return new JsonParser()
+                    .parse(getResponse())
+                    .getAsJsonObject();
+        }
+
     }
 
-    public Request getRequest(URL url) {
-        return new Request(url);
+    /**
+     * create a {@link Connection} instance
+     *
+     * @param url the url refer to the resource which you want to query
+     * @return a {@link Connection} instance
+     */
+    public Connection createConnection(URL url) {
+        return new Connection(url);
     }
 
-    public Request getRequest() {
-        return new Request(mRoot);
+    /**
+     * create a {@link Connection} instance
+     * @return a {@link Connection} instance refer to the root of this {@link Core}
+     */
+    public Connection createConnection() {
+        return new Connection(mRoot);
     }
 
 }
